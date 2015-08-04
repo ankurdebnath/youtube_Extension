a5cfa1c415a63becbc9e9f0a2a8ded62d91d61f8
ankurdebnath <ankurdebnath1994@gmail.com>
2015-08-05 03:29:50 +0530

    update

--- content.js
+++ content.js
@@ -14,22 +14,22 @@ $(document).ready(function() {
 	});
 	var loc;
 	function focus_player () {
-		$(document).ready(function() {
-			window.scrollTo(0, 0);
-			loc = window.location.href;
-			var x = $('button[class="ytp-button ytp-settings-button"]').parent().parent().parent().children('.ytp-panelpopup').children('.ytp-panel').children('.ytp-panel-content').children('.ytp-menu');
-			$('button[class="ytp-button ytp-settings-button"]').click();
-			$('div[class="html5-video-container"]').click();
-			for (var i = 0; i < x.children().length; i++) {
-				if(x.children().eq(i).children().eq(0).html() == "Annotations" && x.children().eq(i).attr('aria-checked') == "true"){
-					x.children().eq(i).children().eq(0).click();
-					$("div#movie_player").tabIndex = 0;
-					$("div#movie_player").focus();
-				}
-			};
-		});
+		window.scrollTo(0, 0);
+		loc = window.location.href;
+		var x = $('button[class="ytp-button ytp-settings-button"]').parent().parent().parent().children('.ytp-panelpopup').children('.ytp-panel').children('.ytp-panel-content').children('.ytp-menu');
+		$('button[class="ytp-button ytp-settings-button"]').click();
+		$('div[class="html5-video-container"]').click();
+		for (var i = 0; i < x.children().length; i++) {
+			if(x.children().eq(i).children().eq(0).html() == "Annotations" && x.children().eq(i).attr('aria-checked') == "true"){
+				x.children().eq(i).children().eq(0).click();
+			}
+		};
+		$("div#movie_player").tabIndex = 0;
+		$("div#movie_player").focus();
 	}
 	setTimeout(focus_player,2000);
+	$("div#movie_player").tabIndex = 0;
+	$("div#movie_player").focus();
 	chrome.runtime.onMessage.addListener(function(request, sender, callback) {
    		if(request == "Changed"){
    			if(window.location.href!=loc){
